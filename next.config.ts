c999d2d0b2875430c1e0765ade765a724bf4b1ba
Thiago Duarte <orium.agc@gmail.com>
2026-07-22 14:30:03 -0300

    feat: rewrite Multi-Zones para /portfolio-alimenticio

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,18 @@ const nextConfig: NextConfig = {
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  async rewrites() {
+    return [
+      {
+        source: "/portfolio-alimenticio",
+        destination: "https://orium-portfolio-alimenticio.vercel.app",
+      },
+      {
+        source: "/portfolio-alimenticio/:path*",
+        destination: "https://orium-portfolio-alimenticio.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
